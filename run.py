2c62c2eeb496cad2222833fad482430a81e62cb4
kenwals <kenwals@gmail.com>
2021-04-08 13:56:36 +0000

    feat flash message added

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 # creates instance of Flask class in variable called app
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # When we try to browse to the root directory, as indicated by the "/", then Flask triggers the index function underneath and returns the "Hello, World" text.
 @app.route("/")
@@ -33,8 +36,10 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        #print(request.form.get("name"))
+        #print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html" , page_title="Contact")
 
 
